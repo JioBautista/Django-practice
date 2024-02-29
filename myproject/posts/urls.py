6280a42bb5ea425fcb73d2d6879f52f2e4a86968
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-02-29 13:25:38 -0500

    Updates

---
+++ myproject/posts/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [path("posts/", views.posts, name="posts")]
--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Post
 
 # Create your views here.
+
+
+def posts(request):
+    posts = Post.objects.all().values()
+    template = loader.get_template("myfirst.html")
+    context = {
+        "posts": posts,
+    }
+    return HttpResponse(template.render(context, request))
